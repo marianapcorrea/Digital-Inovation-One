c31e61d8623a23795c5705b0d8e2b2ab803ce7cb
Mariana Correa <mpcs270391@gmail.com>
2022-01-07 22:51:02 -0300

    Reinsere setup.js

---
+++ Bootcamp-Inter/clone-netflix/js/olw/setup.js
@@ -0,0 +1,17 @@
+//O cifrão indica que é uma extenção JQuery
+$(".owl-carousel").owlCarousel({
+  loop: false,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
